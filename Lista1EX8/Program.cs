c500616083fd2fbcbc6904c999f90b6f97bd67d9
ALUNO <ALUNO@LABHPC22>
2024-02-27 16:36:06 -0300

    Adicionar arquivos de projeto.

---
+++ Lista1EX8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1EX8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, menor = 0, maior = 0;
+            Console.WriteLine("Digite 5 Números: ");
+            a = int.Parse(Console.ReadLine());
+            maior = a;
+            menor = a;
+            int cont = 4;
+
+            while (cont > 0) {
+                b = int.Parse(Console.ReadLine());
+
+                if (b > maior) { maior = b; }
+                if (b < menor) { menor = b; }
+                cont--;
+            }
+            
+            Console.WriteLine("Maior Número: " + maior);
+            Console.WriteLine("Menor Número: " + menor);
+
+
+            Console.ReadKey();
+        }
+    }
+}
